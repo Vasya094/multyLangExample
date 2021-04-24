4625118548d9c6676a91634cfd61533b832a1e1a
Vasya094 <73393463+Vasya094@users.noreply.github.com>
2021-04-24 20:34:30 +0300

    ready multy languages app example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Dropdown } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+import cookies from "js-cookie";
+
+const languages = [
+  { code: "ar", name: "العربية", country_code: "sa", dir: "rtl" },
+  { code: "en", name: "English", country_code: "gb" },
+  { code: "ru", name: "Русский", country_code: "ru" },
+];
 
 function App() {
+  const currentLanguageCode = cookies.get("i18next") || "en";
+  const currentLanguage = languages.find(
+    (lg) => lg.code === currentLanguageCode
+  );
+
+  const { t } = useTranslation();
+  const releaseDate = new Date("2021-04-23");
+  const timeDifference = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir || "ltr";
+    document.title = t("app_title")
+  }, [currentLanguage, t]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Dropdown className="d-flex justify-content-end mt-3">
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu as="ul">
+          <Dropdown.Item as='li'><span>{t("language")}</span></Dropdown.Item>
+          {languages.map(({ code, name, country_code }) => (
+            <Dropdown.Item
+              as="button"
+              key={country_code}
+              onClick={() => i18n.changeLanguage( code )}
+              disabled={code === currentLanguageCode}
+            >
+              <span
+                className={`flag-icon flag-icon-${ country_code } mx-2`}
+                style={{opacity: code === currentLanguageCode ? 0.5 : 1}}
+              ></span>
+              {name}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal mb-3">{t("welcome_message")}</h1>
+        <p>{t("days_since_release", { number_of_days })}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import HttpApi from 'i18next-http-backend';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import 'bootstrap/dist/js/bootstrap.js';
+import 'flag-icon-css/css/flag-icon.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import i18n from "i18next";
+import {  initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector) // passes i18n down to react-i18next
+  .use(HttpApi) 
+  .init( {
+    supportedLngs: ["en", "ru", "ar"],
+    fallbackLng: "ru",
+    detection: {
+      order: [
+        "htmlTag",
+        "cookie",
+        "localStorage",
+        "path",
+        "subdomain",
+      ],
+      caches: ["cookie"]
+    },
+    backend: {
+      loadPath: '/locations/{{lng}}/translation.json'
+    },
+    react: {useSuspense: false}
+  } );
+  
+const LoadingMarkUp = () => (
+  <div className="py-4 text-center">
+    <h2>Loadng...</h2>
+    </div>
+  )
+
 
 ReactDOM.render(
+  <Suspense fallback={LoadingMarkUp} >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </React.StrictMode>
+    </Suspense>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
